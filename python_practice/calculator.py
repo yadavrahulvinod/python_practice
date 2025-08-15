36f9f2ebfd089e92602646545f2ef60e51dd3d46
yadavrahulvinod <yadavrahulvinod@gmail.com>
2025-08-15 12:10:12 +0530

    mian

---
+++ python_practice/calculator.py
@@ -0,0 +1,11 @@
+print("for decimal integer operations")
+first_digit= float(input("Enter your first digit"))
+secound_digit= float(input("Enter your secound digit"))
+sum = first_digit+secound_digit
+print("sum "+str(sum))
+
+print("for integer operations")
+first_digit= int(input("Enter your first digit"))
+secound_digit= int(input("Enter your secound digit"))
+sum = first_digit+secound_digit
+print("sum "+str(sum))
---
+++ python_practice/inputr_day1.py
@@ -0,0 +1,13 @@
+your_Name = input("Please Type Your Name")
+print(your_Name)
+
+your_Birthyear = input("Enter Your Bitrth Year")
+your_Age =2025- int (your_Birthyear)
+print(your_Age)
+
+
+# to convert the type of a variable 
+int()
+float()
+str()
+bool()
