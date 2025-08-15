b78d5446638896a96510d86f8a9f3eeae46bdaf3
yadavrahulvinod <yadavrahulvinod@gmail.com>
2025-08-15 08:42:28 +0530

    variables_day1

---
+++ python_practice/variables_day1.py
@@ -0,0 +1,12 @@
+age =20
+age =30.6
+print(age) 
+online = False
+name = "rahul vinod yadav"
+print(age,online,name)
+
+patient_name ="John Smith"
+patient_age = 50
+patient_type ="New"
+
+print("patient_name : ",patient_name,"patient_age : ",patient_age,"patient_type : ",patient_type)
